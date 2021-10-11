55562cd4bcc1a4ac1faa1d1da9898bf7d0b417b1
Pontus Broberg <pobrnetlight.com@Pontuss-MacBook-Pro.local>
2021-10-11 16:13:01 +0200

    initial commit. Hello world done

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = () => {
+    return <div>Hi there!</div>;
+};
+
+// Take the react component and show on the screen
+ReactDOM.render( <App />, document.querySelector('#root'));
