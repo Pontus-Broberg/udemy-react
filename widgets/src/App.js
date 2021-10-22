d90ff6743180cec4f5ef7d89ab33d2a9938b7ddc
Pontus Broberg <pontus.broberg95@gmail.com>
2021-10-22 16:35:06 +0200

    all different widgets done. Section 12 done

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -17,10 +19,25 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "Red",
+    value: "red",
+  },
+  {
+    label: "Green",
+    value: "green",
+  },
+  {
+    label: "Blue",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <Translate />
     </div>
   );
 };
