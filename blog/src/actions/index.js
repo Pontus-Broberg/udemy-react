cb2353bed6b65fcdcf840a11218cf9e2bfda788e
Pontus Broberg <pontus.broberg95@gmail.com>
2021-11-09 12:57:45 +0100

    another done commit

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,37 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispach, getState) => {
+  await dispach(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispach(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async (dispach) => {
   const res = await jsonPlaceholder.get("/posts");
 
-  dispach({ type: "FETCH_POST", payload: res.data });
+  dispach({ type: "FETCH_POSTS", payload: res.data });
 };
+
+export const fetchUser = (id) => async (dispach) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispach({ type: "FETCH_USER", payload: res.data });
+};
+
+// OPTIONAL way to only fetch one user at a time, not optimal tho
+// This will not allow you to re-fetch a user.
+
+// export const fetchUser = (id) => (dispach) => {
+//   _fetchUser(id, dispach);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispach) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispach({ type: "FETCH_USER", payload: res.data });
+// });
